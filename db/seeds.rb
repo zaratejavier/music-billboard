fbd93cdcad2a55bc2e937cc20f4c40ad1dff0c1a
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-24 19:16:26 -0600

    added billboard index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ tops = ['us_100', 'euro_100']
+
+ 10.times do 
+  song = Song.create(name: '')
+ end
